3aec00947602af248f319c98054a1f498ef788da
mike-port <miike.day@gmail.com>
2021-03-25 18:59:53 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const select = document.querySelector('select');
+const para = document.querySelector('p');
+
+select.addEventListener('change', setWeather);
+
+function setWeather() {
+  const choice = select.value;
+
+  if (choice === 'sunny') {
+    para.textContent = 'It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.';
+  } else if (choice === 'rainy') {
+    para.textContent = 'Rain is falling outside; take a rain coat and an umbrella, and don\'t stay out for too long.';
+  } else if (choice === 'snowing') {
+    para.textContent = 'The snow is coming down.'
+  } else if (choice === 'overcast') {
+    para.textContent = 'It is cloudy'
+  } else {
+    para.textContent = '';
+  }
+}
