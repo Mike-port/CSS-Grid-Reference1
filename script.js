ef558514be38275e11b68d17673b2939a114b98a
mike-port <miike.day@gmail.com>
2021-03-27 08:57:13 +0900

    Practicing more

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 const select = document.querySelector('select');
 const para = document.querySelector('p');
 
-select.addEventListener('change', setWeather);
+select.addEventListener('change', setFavoriteFood);
 
-function setWeather() {
+function setFavoriteFood() {
   const choice = select.value;
 
-  if (choice === 'sunny') {
-    para.textContent = 'It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.';
-  } else if (choice === 'rainy') {
-    para.textContent = 'Rain is falling outside; take a rain coat and an umbrella, and don\'t stay out for too long.';
-  } else if (choice === 'snowing') {
-    para.textContent = 'The snow is coming down.'
-  } else if (choice === 'overcast') {
-    para.textContent = 'It is cloudy'
-  } else {
-    para.textContent = '';
+  switch (choice) {
+    case 'pickles':
+      para.textContent = 'Pickles are made from drowned cucumbers. You monster.';
+      break;
+    case 'natto':
+      para.textContent = 'Natto is one of the 7 healthy sins.';
+      break;
+    case 'cheese':
+      para.textContent = 'Cheese is stinky like Britfeet';
+      break;
+    case 'chocolate':
+      para.textContent = 'With coffee, yum.';   
+      break;
+      default:
+        para.textContent = ''; 
   }
 }
